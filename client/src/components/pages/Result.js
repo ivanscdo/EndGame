36e865d1365f7f5b5029d09f77eb278e0cf70a52
Pierce Morrill <piercemorrill91@gmail.com>
2018-06-24 18:29:26 -0500

    Merge branch 'devtest' into origin/develop

--- client/src/components/pages/Result.js
+++ client/src/components/pages/Result.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+function initMap(){
+
+  var startLocation = {lat: 30.2747, lng: -97.7404};
+
+  map = new google.maps.InfoWindow(document.getElementById('map'),{
+    zoom: 12, 
+    center: startLocation,
+    gestureHandling: 'greedy'
+  });
+}
+
 // home to google maps 
 const Result = () =>
   <div>
     <h1>Result</h1>
-    <p>
-     Algotithm gets run and we display result here
-    </p>
+      <div id="map">
+      </div>   
   </div>;
 
 export default Result;
