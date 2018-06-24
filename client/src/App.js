9af6575b95dfbaceb5191dd209aa7107dc595022
snavdeepsingh <snavdeepsingh@gmail.com>
2018-06-24 00:33:34 -0500

    added firebase authentication code for google,facebook,twitter and github

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,66 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./components/pages/Home";
-import Result from "./components/pages/Result";
-import Login from "./components/pages/Login";
-import Invite from "./components/pages/Invite";
+import React, { Component } from "react";
+// import dotenv from 'dotenv';
+// import {FIREBASE_API, FIREBASE_AUTH_DOMAIN} from "dotenv";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Home from "./components/pages/Home";
+// import Result from "./components/pages/Result";
+// import Login from "./components/pages/Login";
+// import Invite from "./components/pages/Invite";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-const App = () =>
-  <Router>
-    <div>
-      <Navbar />
-      <Route exact path="/" component={Login} />
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/invite" component={Invite} />
-      <Route path="/result" component={Result} />
-    </div>
-  </Router>;
+// dotenv.config();
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+})
+
+class App extends Component {
+  state = {isSignedIn: false}
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+   
+
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn:!!user})
+      console.log("user",user);
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+      {this.state.isSignedIn ? (
+        <div>
+        <div>Signed In! </div>
+        <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button>
+        <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
+        <img alt="user" src={firebase.auth().currentUser.photoURL} />
+        </div>
+      ) :
+        (<StyledFirebaseAuth 
+        uiConfig={this.uiConfig}
+        firebaseAuth={firebase.auth()}
+        />)
+        }
+      </div>
+    )
+  }
+}
 
 export default App;
 
