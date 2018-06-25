553e8fc569732d6e5d4dcee6cd477a739e13e031
Pierce Morrill <piercemorrill91@gmail.com>
2018-06-25 17:46:30 -0500

    compiled yesterdays work

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import Home from "./components/pages/Home";
-// import Result from "./components/pages/Result";
+import Navbar from "./components/Navbar";
+import Home from "./components/pages/Home";
+import Result from "./components/pages/Result";
 import Login from "./components/pages/Login";
-// import Invite from "./components/pages/Invite";
+import Invite from "./components/pages/Invite";
 import fb from "./keys";
 import firebase from "firebase";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
@@ -12,10 +12,7 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 // dotenv.config();
 console.log(fb);
 
-firebase.initializeApp({
-  apiKey: fb.key,
-  authDomain: fb.auth
-})
+
 
 class App extends Component {
   state = {isSignedIn: false}
@@ -41,23 +38,24 @@ class App extends Component {
     })
   }
 
+  // this is what will be displayed once signed in
   render(){
     return (
-      <div className="App">
-      {this.state.isSignedIn ? (
-        <div>
-        <div>Signed In! </div>
-        <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button>
-        <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
-        <img alt="user" src={firebase.auth().currentUser.photoURL} />
+        <div className="App">
+          {this.state.isSignedIn ? (
+          <div>
+            <div>Signed In! </div>
+            <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
+            <img alt="user" src={firebase.auth().currentUser.photoURL} />
+          </div>
+        ) :
+          (<StyledFirebaseAuth 
+          uiConfig={this.uiConfig}
+          firebaseAuth={firebase.auth()}
+          />)
+          }
         </div>
-      ) :
-        (<StyledFirebaseAuth 
-        uiConfig={this.uiConfig}
-        firebaseAuth={firebase.auth()}
-        />)
-        }
-      </div>
     )
   }
 }
