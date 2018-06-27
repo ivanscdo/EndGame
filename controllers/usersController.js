28657327ea7b4cc35c06fdb4805bfada4e3d77e6
snavdeepsingh <snavdeepsingh@gmail.com>
2018-06-27 01:47:43 -0500

    added find or create functionality to prevent duplicate users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,14 +4,20 @@ module.exports = {
     findAll: function(req, res) {
       db
         .find(req.query)
-        .sort({ isSignedIn: true })
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
       db
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err));
+        .findOne({email:req.email})
+        .then(currentUser => {
+          if(currentUser){
+            dbModel => res.json(dbModel)
+          }else {
+            db.create(req.body)
+            .then(dbModel => res.json(dbModel))
+          }})
     }
   };
+
+  
