3ea12042235df97ffa8c7ffd786f103cfcdc7e39
John Duck <jduck1133@gmail.com>
2018-06-28 18:18:06 -0500

    Working Commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
 import Result from "./components/pages/Result";
-//import Login from "./components/pages/Login";
+import Login from "./components/pages/Login";
 import Invite from "./components/pages/Invite";
 //import keys from "./keys";
 import firebase from "firebase";
