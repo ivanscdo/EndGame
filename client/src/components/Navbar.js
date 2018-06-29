e4dcc87af98b0edf2c477f16e3ec8ce5ca3f171b
John Duck <jduck1133@gmail.com>
2018-06-28 22:12:44 -0500

    Heroku Fix

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,7 +2,6 @@ import React,{Component} from "react";
 import { Link } from "react-router-dom";
 import Button from '@material-ui/core/Button';
 import Avatar from '@material-ui/core/Avatar';
-import { Link } from "react-router-dom";
 import SvgIcon from '@material-ui/core/SvgIcon';
 import firebase from "firebase";
 import API from "../utils/API";
