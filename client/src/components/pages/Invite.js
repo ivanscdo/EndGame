c6177009fc36acbc56aeb96273b7250ea4c1f6f2
J Pennington <grendelsdj@gmail.com>
2018-06-28 22:07:45 -0500

    merge

--- client/src/components/pages/Invite.js
+++ client/src/components/pages/Invite.js
@@ -1,37 +1,36 @@
+
+
 import React from "react";
 import firebase from "firebase";
 import FriendsList from "../FriendsList";
 import { Paper, Typography } from "@material-ui/core";
 import "./PageBody.css";
 
+
 const styles = {
   Paper: { padding: 20, width: 600 },
-};
-
-
-
-render () { 
-  return (
-
-    <div className='page-body'>
-      <Paper style={styles.Paper}>
-        {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
-          <Typography variant='Title'>
-          <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
-          Welcome {firebase.auth().currentUser.displayName}! You are signed in.
-          </Typography> 
-          <br />
-
-          <Typography variant='display1'>Invite</Typography>
-          </Paper>
-          <br />
-
-          <FriendsList />
-
-          </div>
-
-  )
 }
-}
+
+const Invite = () =>
+
+
+  <div className='page-body'>
+    <Paper style={styles.Paper}>
+      {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
+      <Typography variant='Title'>
+      <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
+        Welcome {firebase.auth().currentUser.displayName}! You are signed in.
+      </Typography> 
+      <br />
+
+        <Typography variant='display1'>Invite</Typography>
+    </Paper>
+    <br />
+
+
+   <FriendsList />
+
+  </div>
+
 
 export default Invite;
