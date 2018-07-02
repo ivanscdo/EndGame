c5b75df5fc7d32e20034df21df7968f38bc2c325
snavdeepsingh <snavdeepsingh@gmail.com>
2018-07-02 09:43:41 -0500

    edited home.js

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 // import FriendsList from "../FriendsList";
 import React, {Component} from "react";
-import firebase from "firebase";
+// import firebase from "firebase";
 import  Map from "./../maps/map.js";
 // import API from "../../utils/API";
 //import keys from "../../keys";
@@ -12,7 +12,7 @@ class Home extends Component {
 
 
   render (){
-  //  console.log("this is process.env",process.env)
+   console.log("this is process.env",process.env)
     return (
             <div className='page-body'>
               <h1>Home</h1>
