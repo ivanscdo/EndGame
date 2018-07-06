bd0b054b9d4d1eed7a8c415affb41477daba1461
snavdeepsingh <snavdeepsingh@gmail.com>
2018-07-05 19:41:56 -0500

    merged with Jonathan's code to invite friends

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
+// import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
 import InviteButton from './InviteButton';
 import List from '@material-ui/core/List';
 import FormGroup from '@material-ui/core/FormGroup';
@@ -20,37 +21,66 @@ function FriendsList (props){
   //users = props.liveUsers;
   // const users = props.liveUsers;
   //["0"].date
-  let users = props.users;
-  console.log(users, "FriendsList Users");
+  // let users = props.users;
+  //console.log(users, "FriendsList Users");
   
     return (
+      
+    <div className='page-body'> 
+      <Paper style={styles.Paper}>
+        <form className=/*{classes.container} noValidate*/ 'valueS'>
+          <TextField
+            id="datetime-local"
+            label="Next appointment"
+            type="datetime-local"
+            defaultValue="2017-05-24T10:30"
+            className=/*{classes.textField}*/ 'valueP'
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form>
+        <br />
+      </Paper>    
+
+    <br />
+
       <Paper style={styles.Paper}>
         <Typography variant="title" className=/*{classes.title}*/'valueL'>
             Your Available Friends:
         </Typography>
             <div className=/*{classes.demo}*/'valueD'>
-                  <List /*dense={dense}*/ className='valueK'>
-                    <FormControl component="fieldset">
-                    <FormLabel component="legend"></FormLabel>
-                    <FormGroup>
-                      {props.users.map((item, index ) => (
-                        <FriendsListItem 
-                        key = {item._id}
-                        photoURL={item.photoURL}
-                        userName={item.userName}
-                        email={item.email}
-                        online={item.isSignedIn}
-                        checked={props.handleChange}
-                        >
-                        </FriendsListItem>
-                      ))}
-                    </FormGroup>
-                    </FormControl>
+              <form onSubmit={props.submit}>
+                    <List /*dense={dense}*/ className='valueK'>
+                    
+                      <FormControl component="fieldset"> 
+                      <FormLabel component="legend">Meet UP Instance</FormLabel>
+                      <FormGroup>
+                        {props.users.map((item, index) => (
+                          <FriendsListItem 
+                          key = {item._id}
+                          photoURL={item.photoURL}
+                          userName={item.userName}
+                          email={item.email}
+                          online={item.isSignedIn}
+                          checked={props.checked}
+                          handleChange={props.handleChange}
+                          >
+                          </FriendsListItem>
+                        ))}
+                      </FormGroup>
+                      <InviteButton/>
+                      {/* <button 
+                      type= "submit"
+                      >
+                      Submit
+                      </button> */}
+                      </FormControl>
                   </List>
-                  </div>
-          <InviteButton />
+                 </form> 
+            </div>
       </Paper>
-
+    </div>
     )
 }
 
--- client/src/components/pages/Invite.js
+++ client/src/components/pages/Invite.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import firebase from "firebase";
 import FriendsList from "../FriendsList";
 // import InviteButton from '../InviteButton';
-import TimePicker from "../TimePicker";
-import { Paper, Typography, Grid } from "@material-ui/core";
+// import TimePicker from "../TimePicker";
+import { Paper, Typography } from "@material-ui/core";
 import "./PageBody.css";
 import API from "../../utils/API";
 
@@ -11,75 +11,96 @@ const styles = {
   Paper: { padding: 20, width: 600, margin: 'auto' },
 }
 
-  class Invite extends Component { 
-    constructor(props) {
-      super(props); 
-      this.state = {
-        isSignedIn: true,
-        user: {},
-        value: 0,
-        checked: false,
-        liveUsers: [], 
-      };
+class Invite extends Component { 
+  constructor(props) {
+    super(props); 
+    this.state = {
+      isSignedIn: true,
+      user: {},
+      group: [],
+      checked: false,
+      liveUsers: [],
+      handleChange: this.handleChange
     };
-  
-    handleChange = name => event => {
-      this.setState({ [name]: event.target.checked = true });
-    };
-  
-    componentDidMount() {
-      API.getUsers()
-      .then((response)=> {
-        this.setState(()=>{
-          return {
-            liveUsers: response.data
-          }
-        })
-      })
-      .catch(err=>console.log(err))
-    }    
-    
-    
-    render(){
-      let users = this.state.liveUsers;
-      return (
-          <div className='page-body'>
-            <Grid container spacing={24}> 
-            <Grid item xs={12}> 
-                <Paper style={styles.Paper}>
-                  {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
-                    <Typography variant='Title'>
-                    <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
-                    Welcome {firebase.auth().currentUser.displayName}! You are signed in.
-                    </Typography> 
-                <br />
+  };
 
-              <Typography variant='display1'>Invite</Typography>
-              </Paper>
-            </Grid>
-            <br />
-            <Grid item xs={12}>
-              <TimePicker />    
-            </Grid>
-            <br />
-            <Grid item xs={12}>
-            <FriendsList 
-              users = {this.state.liveUsers}
-              handleChange = {this.state.handleChange}
-            />
-
-            </Grid>
-
-             <Grid item xs={12}>
-
-
-            </Grid>
-
-            </Grid>
-          </div>
-        )
-      }
+  componentWillUpdate(nextProps, nextState){
+    console.log("Invite Js line 27", nextState);
+    // this.props.groupStateUpdate(this.state.group);
   }
 
+  handleGroupSubmit = (e) => {
+    e.preventDefault();
+    console.log("In Submit Function");
+   
+  }
+  
 
+  handleChange = event => {
+    var tempArr= [];
+    console.log(event.target);
+    if(this.state.checked === false){
+      tempArr= [...this.state.group, event.target.value]
+      this.setState({ 
+        group: tempArr,
+        checked: true
+      });
+    } else if (this.state.checked === true) {
+      tempArr = [...this.state.group];
+      let index = tempArr.indexOf(event.target.value);
+      tempArr.splice(index, 1);
+      this.setState({
+        group: tempArr,
+        checked: false
+      });
+    }
+    console.log("this is tempARRY",tempArr);
+    console.log(this.state.group);
+  };
+
+  componentDidMount() {
+    API.getUsers()
+    .then((response)=> {
+      this.setState(()=>{
+        return {
+          liveUsers: response.data
+        }
+      })
+    })
+    .catch(err=>console.log(err))
+  }    
+    
+    render(){
+      
+      let users = this.state.liveUsers;
+      console.log(users, "Invite JS");
+      return (
+        <div className='page-body'>
+          <Paper style={styles.Paper}>
+            {/* <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button> */}
+            <Typography variant='Title'>
+              <img alt="user" width="50px" margin='5px'src={firebase.auth().currentUser.photoURL} />
+              Welcome {firebase.auth().currentUser.displayName}! You are signed in.
+            </Typography> 
+          <br/>
+            <Typography variant='display1'>Invite</Typography>
+          </Paper>
+          <br />
+          <FriendsList
+           users = {this.state.liveUsers}
+           handleChange = {this.handleChange}
+           checked = {this.checked}
+           submit = {this.handleGroupSubmit}
+           />
+        </div>
+      )
+    }
+  }
+
+  //           <Inputs />
+
+  //         </div>
+  //       )
+  //     }
+  // }
 export default Invite;
