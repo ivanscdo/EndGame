e55558f861ce86e9383be1ea4cd23ee8e73b66cd
Ivan Saucedo <ivan.scdo@gmail.com>
2018-07-08 13:55:32 -0500

    su0708.13:57 added material ui grid

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,37 +1,43 @@
 import React, { Component } from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import Navbar from "../Navbar";
 import Home from "./Home";
 import Invite from "./Invite";
 import Result from "./Result";
-import Navbar from "../Navbar";
-import firebase from "firebase";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import API from "../../utils/API";
 import { Paper, Typography } from '@material-ui/core';
-import './PageBody.css';
+// import './PageBody.css';
 // import keys from "../../keys";
+import Grid from '@material-ui/core/Grid';
+// import './firebaseui-styling.global.css';
+// import styles from "./firebaseAuth.css";
+
 
 firebase.initializeApp({
   apiKey: "AIzaSyDYTXe8VuIi0gdZVfI1V1kHpJ2N9Xj23-I",
   authDomain: "endgame-1529521978924.firebaseapp.com"
 })
 
-const styles = {
-  Paper: { 
-    padding: 20, 
-    width: 300, 
-    margin: 'auto', 
-    paddingTop: 30
+// const styles = {
+//   Paper: { 
+//     padding: 20, 
+//     width: 300, 
+//     margin: 'auto', 
+//     paddingTop: 30
 
 
-  },
-}
+//   },
+// }
 
 class Login extends Component {
+
   state = {
     isSignedIn: false,
     user: {}
   }
+
   uiConfig = {
     signInFlow: "popup",
     signInSuccessUrl: window.location,
@@ -76,42 +82,83 @@ class Login extends Component {
     //   )
     //   .catch(err => console.log(err));
     // }
-    render(){
-      return (
-        <div className="App">
+  render(){
+    return (
+      <div className="App">
         {this.state.isSignedIn ? (
-          
-        <Router>
-          <div>
-            <Navbar />
-            <Route  exact path="/" component={Home} />
-            <Route exact path="/invite" component={Invite} />
-            <Route exact path="/result" component={Result} />
-          </div>
-        </Router>
-          // <div>
-          // <div>Signed In! </div>
-          // <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button>
-          // <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
-          // <img alt="user" src={firebase.auth().currentUser.photoURL} />
-          // </div>
-        ) :
-          (
-            <Paper style={styles.Paper}>
-              <div className='logo-box' align='center' bottom-margin='10'>
-                <img src='./eglogo.png' width='200'/>
-                <StyledFirebaseAuth 
+          <Router>
+            <div>
+              <Navbar />
+              <Route  exact path="/" component={Home} />
+              <Route exact path="/invite" component={Invite} />
+              <Route exact path="/result" component={Result} />
+            </div>
+          </Router>
+            // <div>
+            // <div>Signed In! </div>
+            // <button className="btn" onClick={()=>firebase.auth().signOut()}> Sign out!</button>
+            // <h1>Welcome {firebase.auth().currentUser.displayName} </h1>
+            // <img alt="user" src={firebase.auth().currentUser.photoURL} />
+            // </div>
+        ) : (
+          // <Paper
+          //   // style={
+          //   //   // styles.Paper
+          //   // }
+          // >
+            // <div
+            //   // className='logo-box'
+            //   // align='center'
+            //   // bottom-margin='10'
+            // >
+            //   <img
+            //     src='./eglogo.png'
+            //     width='200'
+            //   />
+            //   <StyledFirebaseAuth 
+            //     uiConfig={this.uiConfig}
+            //     firebaseAuth={firebase.auth()}
+            //   />
+            // </div>
+          // </Paper>
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justify="center"
+          >
+          <Grid item></Grid>
+            <Paper>
+              <Grid item>
+                <Grid
+                  container
+                  alignItems="center"
+                  justify="center"
+                > 
+                  <Grid item>
+                    <img
+                      src='./eglogo.png'
+                      width='200'
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <StyledFirebaseAuth
                   uiConfig={this.uiConfig}
                   firebaseAuth={firebase.auth()}
-                />
-              </div>
+                />  
+              </Grid>
             </Paper>
-           )
-          }
-        </div>
-      )
-    }
+          <Grid item></Grid>
+
+          </Grid>
+
+        )}
+      </div>
+    )
   }
+}
   
   export default Login;
 
